e63e9c9fc9a04e5fee7e4465cb931530444d6b9a
salistoreoficial-svg <salistoreoficial@gmail.com>
2026-08-22 15:39:02 -0400

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,16 @@
+const produtos = [
+  {
+    id: 1,
+    nome: "Vestido SALI",
+    preco: 49.99,
+    imagem: "AB0ECD64-5FFA-4265-92D5-9167066B51FE.png",
+    categoria: "Vestidos"
+  },
+  {
+    id: 2,
+    nome: "Look SALI",
+    preco: 49.99,
+    imagem: "1F51613E-DBE2-4D72-83EC-C461362D065D.jpeg",
+    categoria: "Conjuntos"
+  }
+];
