a20bbece17cced2644c5cb975bca59218c0a8c1b
salistoreoficial-svg <salistoreoficial@gmail.com>
2026-08-24 15:52:12 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "sali-store-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./produtos.js",
+  "./manifest.json",
+  "./815BCB89-C59E-479D-83FF-454243870EB3.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARQUIVOS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(chaves => {
+      return Promise.all(
+        chaves
+          .filter(chave => chave !== CACHE_NAME)
+          .map(chave => caches.delete(chave))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(resposta => {
+        const copia = resposta.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copia));
+
+        return resposta;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
