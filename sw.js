41e14424898fbb6f00d67226d9f3bf2635a91172
salistoreoficial-svg <salistoreoficial@gmail.com>
2026-09-21 23:08:01 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,19 @@
-const CACHE_NAME = "tropicanaz-v7-3";
-
-const ARQUIVOS = [
-  "./",
-  "./index.html",
-  "./produtos.js",
-  "./manifest.json",
-  "./tropicanaz-v7-3",
-  "./tropicanaz-v7-3"
-];
+const CACHE_NAME = "tropicanaz-v7-4-clean";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ARQUIVOS))
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(chaves => {
-      return Promise.all(
-        chaves
-          .filter(chave => chave !== CACHE_NAME)
-          .map(chave => caches.delete(chave))
-      );
-    })
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
+// Sempre busca a versão atual na rede. Não mantém HTML/configurações em cache.
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(resposta => {
-        const copia = resposta.clone();
-
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copia));
-
-        return resposta;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
